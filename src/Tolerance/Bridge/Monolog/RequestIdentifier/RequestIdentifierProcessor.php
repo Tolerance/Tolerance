8803a7a6b38dae9e9c71294ab0503db15ab5e794
Samuel ROZE <samuel.roze@gmail.com>
2016-01-05 18:56:57 +0000

    Add the monolog integration

---
+++ src/Tolerance/Bridge/Monolog/RequestIdentifier/RequestIdentifierProcessor.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Tolerance package.
+ *
+ * (c) Samuel ROZE <samuel.roze@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tolerance\Bridge\Monolog\RequestIdentifier;
+
+use Tolerance\RequestIdentifier\Resolver\RequestIdentifierResolver;
+
+class RequestIdentifierProcessor
+{
+    /**
+     * @var RequestIdentifierResolver
+     */
+    private $requestIdentifierResolver;
+
+    /**
+     * @param RequestIdentifierResolver $requestIdentifierResolver
+     */
+    public function __construct(RequestIdentifierResolver $requestIdentifierResolver)
+    {
+        $this->requestIdentifierResolver = $requestIdentifierResolver;
+    }
+
+    /**
+     * Updates the record with the request identifier.
+     *
+     * @param array $record
+     *
+     * @return array
+     */
+    public function __invoke(array $record)
+    {
+        $record['context']['tags'] = [
+            'request-identifier' => $this->requestIdentifierResolver->get()->get(),
+        ];
+
+        return $record;
+    }
+}
