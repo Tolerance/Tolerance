c3735fb05b5feb0cac97e3008577a1492df88bc6
Théo FIDRY <theo.fidry@gmail.com>
2016-04-27 19:59:14 +0100

    Added providers

---
+++ src/Tolerance/Bridge/Laravel/Provider/MessageProfile/JmsSerializerProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tolerance\Bridge\Laravel\Provider\MessageProfile;
+
+use Tolerance\Bridge\Laravel\Illuminate\Support\ServiceProvider;
+
+final class JmsSerializerProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        //TODO
+    }
+}
--- src/Tolerance/Bridge/Laravel/Provider/ToleranceProvider.php
+++ src/Tolerance/Bridge/Laravel/Provider/ToleranceProvider.php
@@ -14,6 +14,14 @@ final class ToleranceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->register(MessageProfile\Storage\BufferedProvider::class);
+
+        $this->app->register(MessageProfile\GuzzleProvider::class);
+        $this->app->register(MessageProfile\JmsSerializerProvider::class);
+        $this->app->register(MessageProfile\ListenerProvider::class);
+        $this->app->register(MessageProfile\MonologProvider::class);
+        $this->app->register(MessageProfile\StorageProvider::class);
+
         $this->app->register(Operation\AopProvider::class);
         $this->app->register(Operation\ListenerProvider::class);
 
